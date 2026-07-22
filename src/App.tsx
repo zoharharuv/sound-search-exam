e36a862e9fda819cee34bcd4a85429872502a8a8
zoharharuv <zoharpr0@gmail.com>
2026-07-22 11:09:39 +0300

    milestone 1 - base app.tsx and main.tsx

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import { SoundSearchPage } from "@/pages/SoundSearchPage";
+
+export default function App() {
+  return <SoundSearchPage />;
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import App from "./App";
+import { store } from "./store/store";
 import "./index.css";
+
+const queryClient = new QueryClient();
 
 const rootElement = document.getElementById("root");
 
@@ -10,8 +16,10 @@ if (!rootElement) {
 
 createRoot(rootElement).render(
   <StrictMode>
-    <main>
-      <h1>Sound Search</h1>
-    </main>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </Provider>
   </StrictMode>,
 );
